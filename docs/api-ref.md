95557ac1a07616baa83059e57a0ec7fb40a2122d
clam119 <christopher.lam119@gmail.com>
2022-10-29 15:43:04 +0100

    Add current template which has a blank slate of no other docs and a homepage for API reference.

---
+++ docs/api-ref.md
@@ -0,0 +1,30 @@
+---
+id: api-ref
+sidebar_position: 1
+slug: /
+---
+
+# API Reference
+
+## Available endpoints
+
+
+- [GET /api](/docs/Api)
+
+- [GET /api/topics](docs/Topics/get-topics)
+- [POST /api/topics](docs/Topics/post-topics)
+
+- [GET /api/articles](docs/Articles/get-articles)
+- [POST /api/articles](docs/Articles/post-articles)
+- [GET /api/articles/:article_id](docs/Articles/get-articles-by-id)
+- [PATCH /api/articles/:article_id](docs/Articles/patch-articles-by-id)
+- [DELETE /api/articles/:article_id](docs/Articles/delete-articles-by-id)
+
+- [GET /api/articles/:article_id/comments](docs/Articles/get-article-comments)
+- [POST /api/articles/:article_id/comments](docs/Articles/post-article-comment)
+  
+- [DELETE /api/comments/:comment_id](docs/Comments/delete-comment-by-id)
+- [PATCH /api/comments/:comment_id](docs/Comments/patch-comment-by-id)
+
+- [GET /api/users](docs/Users/get-users)
+- [GET /api/users/:username](docs/Users/get-users-by-username)
---
+++ docusaurus.config.js
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'The tagline of my site',
+  url: 'https://clam119-northcoders-news-api.herokuapp.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/nclogo.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath:'/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/clam119/Northcoders-News-API/docs/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Northcoders News API',
+        logo: {
+          alt: 'Northcoders Official Logo',
+          src: 'img/nclogo.png',
+        },
+        items: [
+          {
+            href: 'https://github.com/clam119',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Learn',
+            items: [
+              {
+                label: 'Style Guide',
+                to: 'docs/',
+              },
+              {
+                label: 'Second Doc',
+                to: 'docs/doc2',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: 'blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'Legal',
+            // Please do not remove the privacy and terms, it's a legal requirement.
+            items: [
+              {
+                label: 'Privacy',
+                href: 'https://opensource.fb.com/legal/privacy/',
+              },
+              {
+                label: 'Terms',
+                href: 'https://opensource.fb.com/legal/terms/',
+              },
+              {
+                label: 'Data Policy',
+                href: 'https://opensource.fb.com/legal/data-policy/',
+              },
+              {
+                label: 'Cookie Policy',
+                href: 'https://opensource.fb.com/legal/cookie-policy/',
+              },
+            ],
+          },
+        ],
+        logo: {
+          alt: 'Meta Open Source Logo',
+          // This default includes a positive & negative version, allowing for
+          // appropriate use depending on your site's style.
+          src: '/img/meta_opensource_logo_negative.svg',
+          href: 'https://opensource.fb.com',
+        },
+        // Please do not remove the credits, help to publicize Docusaurus :)
+        copyright: `Copyright © ${new Date().getFullYear()} Meta Platforms, Inc. Built with Docusaurus.`,
+      },
+    }),
+};
+
+module.exports = config;
